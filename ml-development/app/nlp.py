9f692e573468ff0ea644d4b5e7d9ed7a237368c8
Fathinah Izzati <fathinah@Fathinahs-MacBook-Pro.local>
2021-08-19 16:12:13 +0700

    implement the model inference's api

--- ml-development/app/nlp.py
+++ ml-development/app/nlp.py
@@ -1,22 +1,18 @@
 from question_generator import QuestionGenerator
-from question_generator import print_qa
 import torch
 import time
-print("IN")
+
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # assert device == torch.device('cuda'), "Not using CUDA. Set: Runtime > Change runtime type > Hardware Accelerator: GPU"
 
-qg = QuestionGenerator()
-
-with open('../articles/indian_matchmaking.txt', 'r') as a:
-    article = a.read()
-
-start = time.time()
-qa_list = qg.generate(
-    article, 
-    num_questions=10, 
-    answer_style='all'
-)
-print(str((time.time()-start)/60)+" mins")
-print()
-print_qa(qa_list)
+class NLP:
+    def __init__(self):
+        self.qg = QuestionGenerator()
+        
+    def generate(self, text, num=1, mode='all'):
+        self.qa_list = self.qg.generate(
+            text, 
+            num_questions=num, 
+            answer_style=mode
+        )
+        return self.qa_list
