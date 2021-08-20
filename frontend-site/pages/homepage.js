6fff1a89720b44ca8e197a0215377cc8941bd629
Firdaus Alghifari <firdaus.alghifari@gmail.com>
2021-08-20 18:47:33 +0700

    feat fe: add homepage page

---
+++ frontend-site/pages/homepage.js
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout"
+import SEO from '../components/SEO'
+
+export default function Homepage() {
+  return (
+    <Layout transparent>
+      <SEO title={"Homepage - Question Generator"} />
+      <div>
+        Lorem ipsum
+      </div>
+    </Layout>
+  )
+}
