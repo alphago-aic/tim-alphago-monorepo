a848d0fc783893ca42d08337233b8d84e9e20fdc
Fathinah Izzati <fathinahai@gmail.com>
2021-08-20 11:48:55 +0700

    add dockerfile, fix api

--- ml-development/app/main.py
+++ ml-development/app/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
+from typing import Optional
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from nlp import NLP
-from question_generator import print_qa
+from .nlp import NLP
+from .question_generator import print_qa
 
 class Message(BaseModel):
     '''Setting up pydantic object to handle typing for request messages'''
     input: str
+    num: Optional[int] = 1
+    mode: Optional[str] = 'all'
     output: str = None
 
 # with open('../articles/indian_matchmaking.txt', 'r') as a:
@@ -15,19 +18,19 @@ class Message(BaseModel):
 app = FastAPI()
 nlp = NLP()
 
-origins = ["http://localhost",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000"]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = origins,
+    allow_origins = ["*"],
     allow_credentials=True,
-    allow_methods=['POST'],
+    allow_methods=['*'],
     allow_headers=['*'],
 )
 
 @app.post("/generative/")
-async def  generate(message: Message, num: int, mode: str):
-    message.output  = nlp.generate(message.input, num=num, mode=mode)
+async def  generate(message: Message):
+    message.output  = nlp.generate(message.input, num=message.num, mode=message.mode)
     return {"output" : message.output}
+
+@app.get('/')
+async def main():
+    return {"output":"Hello World!?"}
