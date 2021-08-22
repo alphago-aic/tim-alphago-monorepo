7d0b648d5976a919a18164bdb59b37325b56d7ee
galacticor <widyantohadi@gmail.com>
2021-08-22 20:24:27 +0700

    add cors

--- ml-deployment/alphago/__init__.py
+++ ml-deployment/alphago/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from alphago.qg.interface import IQGService, QGSpec
 from alphago.di import injector
@@ -6,6 +7,13 @@ from .response import BaseResponse
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 @app.get("/ping")
 async def ping():
