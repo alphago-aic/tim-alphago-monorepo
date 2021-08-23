54dcbfa4e3259d8944ca705985f2fc30ecbec28c
Firdaus Alghifari <firdaus.alghifari@gmail.com>
2021-08-23 12:35:12 +0700

    bug fe: fix redirects (4)

--- frontend-site/next.config.js
+++ frontend-site/next.config.js
@@ -3,11 +3,11 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/generate-english-qa/:path*',
+        source: 'https://sqna.xyz/generate-english-qa/:path*',
         destination: 'http://34.87.146.224:8000/:path*',
       },
       {
-        source: '/generate-indonesia-qa/:path*',
+        source: 'https://sqna.xyz/generate-indonesia-qa/:path*',
         destination: 'http://sqna.us-east-1.elasticbeanstalk.com/:path*',
       },
     ]
