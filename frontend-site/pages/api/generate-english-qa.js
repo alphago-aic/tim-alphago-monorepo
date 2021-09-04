4a9e3bacae5a7f7cee78a3b1fcdaac4c238487a5
Firdaus Alghifari <firdaus.alghifari@gmail.com>
2021-09-04 20:00:00 +0700

    [GREEN] remove unneeded Next API, confirm https is ready

--- frontend-site/pages/api/generate-english-qa.js
+++ frontend-site/pages/api/generate-english-qa.js
@@ -3,15 +3,15 @@
 const fetch = require('node-fetch');
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const resp = await fetch('http://api.sqna.xyz/api2-generative/', {
-      method: 'POST',
-      body: JSON.stringify(req.body),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    })
-    const data = await resp.json()
-    res.status(200).json(data)
-  }
+  // if (req.method === 'POST') {
+  //   const resp = await fetch('http://api.sqna.xyz/api2-generative/', {
+  //     method: 'POST',
+  //     body: JSON.stringify(req.body),
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //   })
+  //   const data = await resp.json()
+  //   res.status(200).json(data)
+  // }
 }
